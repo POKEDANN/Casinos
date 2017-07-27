2c9708c90a547cc1a42aa9f15fd5126f665d65aa
Daniel Cruz <cruzmmh@gmail.com>
2017-07-26 23:13:33 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,18 @@
+//aqui empieza el casino
+var casino = alert("Bienvenido a Casinito");
+var apuesta = prompt("Escribe tu apuesta");
+function numeroRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+var dadoUno = numeroRandom(1,6);
+alert("el dadoUno salio " + dadoUno);
+if(dadoUno == 2 || dadoUno == 4 || dadoUno == 6){
+	alert("felicidades, va el segundo tiro");
+	var dadoDos = numeroRandom(1,6);
+	alert("el dadoDos salio " + dadoDos);
+	var resultado = apuesta * (dadoDos * dadoUno);
+	alert("tu apuesta ahora es: " + resultado);
+} else {
+	alert("El juego ha terminado, suerte para la proxima, tomate una coca");
+}
+//aqui acaba el casino
